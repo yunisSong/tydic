dfcdc17cc0cf33ae7829bbda20af6beb28b70be8
Yulia <tan@YuliadeMacBook-Pro.local>
2023-09-05 17:00:02 +0800

    首页

--- crawler-ui/src/main.js
+++ crawler-ui/src/main.js
@@ -4,14 +4,11 @@ import './style.css'
 import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import router from './router/Router.js'
-
 import 'element-plus/dist/index.css'
 import App from './App.vue'
-
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 app.use(ElementPlus).use(router)
-
 app.mount('#app')
